c63f5a9f5d96395137d39d88785931615e1437e4
sho endo <endo.sho@moneyforward.co.jp>
2019-09-17 20:13:21 +0900

    Implement sample asset code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,12 @@ import '../src/application.css'
 require.context('../images', true, /\.(png|jpg|jpeg|svg)$/);
 
 console.log('Hello World from Webpacker');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementsByName('text-change-btn')[0];
+  btn.addEventListener('click', () => {
+    const text = document.getElementById('heading');
+    text.innerText = 'Hello, webpacker!';
+    text.style.color = 'blue';
+  })
+});
