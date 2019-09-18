c077ba9568c2e43ca410b6d160ed1d420785dcea
sho endo <endo.sho@moneyforward.co.jp>
2019-09-18 12:58:16 +0900

    Create a stopwatch page

---
+++ app/controllers/stopwatch_controller.rb
@@ -0,0 +1,4 @@
+class StopwatchController < ApplicationController
+  def top
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#top'
   get '/counter', to: 'counter#top'
+  get '/stopwatch', to: 'stopwatch#top'
 end
