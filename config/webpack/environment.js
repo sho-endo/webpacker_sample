143b06b6b43270c62a7268968e2fd3f09c6a0640
sho endo <endo.sho@moneyforward.co.jp>
2019-10-02 19:23:25 +0900

    Import material-components-web.scss via yarn package

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+// ref
+// https://github.com/rails/webpacker/issues/321#issuecomment-329119714
+// https://github.com/rails/webpacker/blob/3-x-stable/docs/webpack.md#overriding-loader-options-in-webpack-3-for-css-modules-etc
+const merge = require('webpack-merge')
+
+  const sassLoaderOptions = {
+    includePaths: environment.toWebpackConfig().resolveLoader.modules
+}
+
+const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === 'sass-loader')
+
+CSSLoader.options = merge(CSSLoader.options, sassLoaderOptions)
+
 module.exports = environment
